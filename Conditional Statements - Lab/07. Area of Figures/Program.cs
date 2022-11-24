aa4c61155aadcb2a277408ee58b796f14ccfadfb
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-24 22:19:19 +0200

    Conditional Statements - Lab
    Conditional Statements - Lab

---
+++ Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string form = Console.ReadLine();
+
+            if (form == "square")
+            {
+                double squareSize = double.Parse(Console.ReadLine());
+                double squareAnswer = squareSize * squareSize;
+                Console.WriteLine($"{squareAnswer:f3}");
+            }
+
+            else if (form == "rectangle")
+            {
+                double rectangleSize = double.Parse(Console.ReadLine());
+                double rectangleSize2 = double.Parse(Console.ReadLine());
+                double rectangleAnswer = rectangleSize * rectangleSize2;
+                Console.WriteLine($"{rectangleAnswer:f3}");
+            }
+
+            else if (form == "circle")
+            {
+                double circleRadius = double.Parse(Console.ReadLine());
+                double circleAnswer = Math.PI * (circleRadius * circleRadius);
+                Console.WriteLine($"{circleAnswer:f3}");
+            }
+
+            else if (form == "triangle")
+            {
+                double triangleSide = double.Parse(Console.ReadLine());
+                double triangleHeight = double.Parse(Console.ReadLine());
+                double circleAnswer = (triangleSide * triangleHeight) / 2;
+                Console.WriteLine($"{circleAnswer:f3}");
+            }
+        }
+    }
+}
